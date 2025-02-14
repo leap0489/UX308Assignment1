0715c8338a79de1c09753e7b5eebb54416fcfe38
leap0489 <144965129+leap0489@users.noreply.github.com>
2025-02-14 01:58:11 +0000

    set up first tests

--- spec/order_spec.js
+++ spec/order_spec.js
@@ -4,19 +4,19 @@ describe("Tests all stages of an order", function() {
     it("test hello", function() {
         const oOrder = new Order("999-999-9999");
         const aResults = oOrder.handleInput("hello");
-        expect(aResults[0]).toBe("Welcome to Rich's Acton Rapid Test.")
+        expect(aResults[0]).toBe("Welcome to Laynol's Chicken Tender Shop!")
     });
     it("test yes", function() {
         const oOrder = new Order("999-999-9999");
         oOrder.handleInput("hello")
         const aResults = oOrder.handleInput("yes");
-        expect(aResults[0]).toBe("Your rapid test is reserved under the phone number 999-999-9999")
+        expect(aResults[0]).toBe("Awesome, our tenders come in classic or spicy. Which would you like?")
     });
     it("test no", function() {
         const oOrder = new Order("999-999-9999");
         oOrder.handleInput("hello")
         const aResults = oOrder.handleInput("no");
-        expect(aResults[0]).toBe("Thanks for trying our reservation system")
+        expect(aResults[0]).toBe("Thanks! Maybe next time :)")
     });
   });
   
