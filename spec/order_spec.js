5fdf89d763e088bed27246882ead8b7abb6080f2
leap0489 <144965129+leap0489@users.noreply.github.com>
2025-02-14 02:17:14 +0000

    add quantity and glaze spec test

--- spec/order_spec.js
+++ spec/order_spec.js
@@ -32,6 +32,40 @@ describe("Tests all stages of an order", function() {
         const aResults2 = oOrder.handleInput("classic");
         expect(aResults2[0]).toBe("Classic it is! Do you want a 3 or 5 piece")
     });
+    it("test 3", function() {
+        const oOrder = new Order("999-999-9999");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults2 = oOrder.handleInput("classic");
+        const aResults3 = oOrder.handleInput("3");
+        expect(aResults3[0]).toBe("3 pieces sounds fantastic, do you want to toss your chicken in our world famous honey BBQ glaze?")
+    });
+    it("test 5", function() {
+        const oOrder = new Order("999-999-9999");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults2 = oOrder.handleInput("classic");
+        const aResults3 = oOrder.handleInput("5");
+        expect(aResults3[0]).toBe("5 pieces sounds fantastic, do you want to toss your chicken in our world famous honey BBQ glaze?")
+    });
+    it("test with honey BBQ glaze", function() {
+        const oOrder = new Order("999-999-9999");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults2 = oOrder.handleInput("classic");
+        const aResults3 = oOrder.handleInput("5");
+        const aResults4 = oOrder.handleInput("yes");
+        expect(aResults4[0]).toBe("Honey BBQ all the way! Do you want to add fries to your chicken?")
+    });
+    it("test without honey BBQ glaze", function() {
+        const oOrder = new Order("999-999-9999");
+        oOrder.handleInput("hello")
+        const aResults = oOrder.handleInput("yes");
+        const aResults2 = oOrder.handleInput("classic");
+        const aResults3 = oOrder.handleInput("5");
+        const aResults4 = oOrder.handleInput("no");
+        expect(aResults4[0]).toBe("No problem. Do you want to add fries to your chicken?")
+    });
   });
   
   
